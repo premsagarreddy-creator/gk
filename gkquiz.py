d158a182fb26639d691567b10fc5e2a6c55b8dd1
premsagarreddy-creator <g.reddy2@s.amity.edu>
2025-05-20 19:54:04 +0530

    rg

--- gkquiz.py
+++ gkquiz.py
@@ -1,11 +1,11 @@
 def gk_quiz():
     """Main function placeholder for General Knowledge Quiz."""
 
-    pppppppass
+    ppppass
 def print_banner();
     print("Welcome to the General Knowledge Quiz!")
     RED = '\033[91m'
-    GREEN = '\033[92m'
+    ggGREEN = '\033[92m'
     YELLOW = '\033[93m'
     CYAN = '\033[96m'
     RESET = '\033[0m'
