a1a487d31b39ae1ecff015e77e4512c76e910fed
premsagarreddy-creator <g.reddy2@s.amity.edu>
2025-05-20 18:08:03 +0530

    Add color constants for styling output

--- gkquiz.py
+++ gkquiz.py
@@ -3,3 +3,8 @@ def gk_quiz():
     pass
 def print_banner():
     print("Welcome to the General Knowledge Quiz!")
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
